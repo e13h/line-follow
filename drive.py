e2ec516347473c12f75054338df01d194481d4b6
Evan Smith <evanphilipsmith@gmail.com>
2022-11-18 17:33:09 -0800

    Copy in image processing to vesc controller

--- drive.py
+++ drive.py
@@ -1,5 +1,10 @@
 import time
 
+import cv2
+import depthai as dai
+
+import toolbox
+
 THROTTLE = 0.0
 
 
@@ -68,5 +73,37 @@ class VESC:
 
 if __name__ == "__main__":
     vesc = VESC('/dev/ttyACM0')
-    vesc.run(0.10, THROTTLE)
-    # TODO does killing the script turn off the vesc or do we need to send a shutdown signal?
+
+    pipeline = dai.Pipeline()
+    camRgb = pipeline.create(dai.node.ColorCamera)
+    xoutRgb = pipeline.create(dai.node.XLinkOut)
+    xoutRgb.setStreamName("rgb")
+    camRgb.setPreviewSize(960, 540)
+    camRgb.setInterleaved(False)
+    camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+    camRgb.preview.link(xoutRgb.input)
+
+    with dai.Device(pipeline, usb2Mode=True) as device:
+        print('Connected cameras: ', device.getConnectedCameras())
+        print('USB speed: ', device.getUsbSpeed().name)
+        if device.getBootloaderVersion() is not None:
+            print('Bootloader version: ', device.getBootloaderVersion())
+
+        qRgb = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+        while True:
+            inRgb = qRgb.get()  # blocking call, will wait until a new data has arrived
+            frame = inRgb.getCvFrame()  # Retrieve 'bgr' (opencv format) frame
+
+            overlay, steering_line = toolbox.run_yellow_segmentation_pipeline(frame)
+            if steering_line is None:
+                overlay, steering_line = toolbox.run_lane_detection_pipeline(frame)
+
+            steering_cmd = toolbox.steering_command(steering_line)
+            print(steering_cmd)
+            vesc.run(steering_cmd, THROTTLE)  # TODO does killing the script turn off the vesc or do we need to send a shutdown signal?
+
+            output = cv2.addWeighted(frame, 0.8, overlay, 1, 1)
+            cv2.imshow("frame", output)
+            if cv2.waitKey(1) == ord('q'):
+                break
+        cv2.destroyAllWindows()
